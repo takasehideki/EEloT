4ed59dbe241df2dce6c93324074eb231a6f81048
takasehideki <takase@i.kyoto-u_ac_jp>
2018-04-17 13:31:49 +0900

    Add measurement.ex

---
+++ measure.ex
@@ -0,0 +1,19 @@
+defmodule Measure do
+  @module_list [[LeibnizFormula, :calc, []], 
+                [Fibonacci, :fib, [100000]]]
+  #@module_list [[Fibonacci, :fib, [100000]]]
+
+  def start do
+    IO.puts "Measurement start.\n"
+    for [module,func,arg] <- @module_list do
+      IO.puts "#{module}, #{func}, #{inspect(arg)}"
+      #{time, ret} = :timer.tc(module, func, arg)
+      {time, _} = :timer.tc(module, func, arg)
+      IO.puts "\ntime\: #{time/1000} ms"
+      #IO.inspect "ret \: #{ret}"
+      IO.puts "----------"
+    end
+  end
+end
+
+Measure.start

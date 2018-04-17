e114d1a6c1ab8ba89b5ad440cfa893d90571cb7a
takasehideki <takase@i.kyoto-u_ac_jp>
2018-04-17 09:38:26 +0900

    Modify funcname in leibniz

--- leibniz_formula.ex
+++ leibniz_formula.ex
@@ -4,7 +4,7 @@ See following link:
 https://qiita.com/hanada/items/c91788bcac2a40f1bb05#comment-b69e6585e1b86b5c03f7
 """
 defmodule LeibnizFormula do
-  def start() do
+  def calc() do
     s = 0..100_000_000
         |> Enum.reduce( 0, fn (x, acc) -> acc + (:math.pow(-1, x) / (2*x + 1)) end )
     IO.puts s*4

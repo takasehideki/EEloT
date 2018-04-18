3c4a3c146e7664aeec4dd8e61d7a92b59debb972
takasehideki <takase@i.kyoto-u_ac_jp>
2018-04-18 10:13:10 +0900

    Add CSVstreaming and PhoenixServer benchmark

--- setup.sh
+++ setup.sh
@@ -1,7 +1,26 @@
-rm Elixir.*.beam
+if [ $# -eq 1 ] && [ $1 == 'clean' ]; then
+  echo "Clean up beam files and git dirs."
+  rm -f Elixir.*.beam
+  rm -rf elixir_agg_csv
+  rm -rf fukuokaex5_csv
+  rm -rf phoenix-showdown
+  exit 0
+fi
 
-elixirc leibniz_formula.exs
-elixirc fibonacci_simple.exs
-elixirc fibonacci_process.exs
+elixirc leibniz_formula.ex
+elixirc fibonacci_simple.ex
+elixirc fibonacci_process.ex
 
+git clone https://github.com/enpedasi/elixir_agg_csv.git
+git clone https://github.com/enpedasi/fukuokaex5_csv.git
+cd elixir_agg_csv/
+7z x ../fukuokaex5_csv/test_300000.7z
+7z x ../fukuokaex5_csv/test_3_000_000.7z
+mix deps.get
+mix compile
+cd ..
 
+git clone https://github.com/mroth/phoenix-showdown.git
+cd phoenix-showdown/phoenix/benchmarker
+mix deps.get
+mix deps.compile
